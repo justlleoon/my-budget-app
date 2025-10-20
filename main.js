0a24173e651cd7341dee9cf82478d9fe54b9d563
lleoon <96005316+justlleoon@users.noreply.github.com>
2025-10-20 20:34:51 +0800

    tiny update :>

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-let user = "Web";
+user = "person"
+//₱
+function defaultvalues(){
+    localStorage.setItem("ws-name-ghjlio","User")
+    localStorage.setItem("ws-svb-ghjlio","20000")
+    localStorage.setItem("ws-wlb-ghjlio","2000")
+    localStorage.setItem("ws-set-ghjlio", true)
+    location.reload();
+}
+if (!localStorage.getItem("ws-set-ghjlio")){
+    defaultvalues();
+}
 let phrase = [
     "Howdy",
     "Hey",
@@ -7,10 +18,15 @@ let phrase = [
     "Hi"
 ]
 function openpane(x){
-    $(".pane").hide()
-    $(x)
+    $("*.pane").hide()
+    $(x).show()
+}
+function onstart(){
+    $(".cashdb h1").text(`₱${localStorage.getItem("ws-wlb-ghjlio")}`)
+    $(".savedb h1").text(`₱${localStorage.getItem("ws-svb-ghjlio")}`)
 }
 function welcomefunc(){$(".texterm h1").text(`${phrase[Math.floor(Math.random() * 4)]}, ${user}!`);}
 $(document).ready(function () {
     welcomefunc()
+    onstart()
 });
